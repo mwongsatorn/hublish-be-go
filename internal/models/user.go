c66bb668ca8b1ad9dde17c7c07ba90db8aa9ed9e
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2024-01-24 20:52:24 +0700

    fix: exclude fields from json marshaling

--- internal/models/user.go
+++ internal/models/user.go
@@ -5,12 +5,12 @@ import "github.com/lib/pq"
 type User struct {
 	CommonFields   `gorm:"embeded"`
 	Username       string         `json:"username" gorm:"not null;unique"`
-	Password       string         `json:"password"`
+	Password       string         `json:"-"`
 	Email          string         `json:"email" gorm:"not null;unique"`
 	Name           string         `json:"name" gorm:"default:''"`
 	Bio            string         `json:"bio" gorm:"default:''"`
 	Image          string         `json:"image" gorm:"default:''"`
-	RefreshTokens  pq.StringArray `json:"refresh_tokens" gorm:"type:text[];default:'{}'"`
+	RefreshTokens  pq.StringArray `json:"-" gorm:"type:text[];default:'{}'"`
 	FollowerCount  uint           `json:"followerCount" gorm:"default:0"`
 	FollowingCount uint           `json:"followingCount" gorm:"default:0"`
 }

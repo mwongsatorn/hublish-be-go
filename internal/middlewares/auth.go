7e39f7d20bfa7948c29b02f1496f63d325aafaa2
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2024-02-03 12:10:27 +0700

    feat: isLoggedIn middleware

--- internal/middlewares/auth.go
+++ internal/middlewares/auth.go
@@ -11,9 +11,26 @@ import (
 var RequireAuth = jwtware.New(jwtware.Config{
 	SigningKey:   jwtware.SigningKey{Key: []byte(os.Getenv("ACCESSTOKEN_KEY"))},
 	Claims:       &types.CustomClaims{},
-	ErrorHandler: errorHandler,
+	ErrorHandler: requireAuthErrorHandler,
 })
 
-func errorHandler(c *fiber.Ctx, e error) error {
+var IsLoggedIn = jwtware.New(jwtware.Config{
+	SigningKey:     jwtware.SigningKey{Key: []byte(os.Getenv("ACCESSTOKEN_KEY"))},
+	Claims:         &types.CustomClaims{},
+	SuccessHandler: isLoggedInSuccessHandler,
+	ErrorHandler:   isLoggedInErrorHandler,
+})
+
+func requireAuthErrorHandler(c *fiber.Ctx, e error) error {
 	return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Token expired."})
+}
+
+func isLoggedInSuccessHandler(c *fiber.Ctx) error {
+	c.Locals("isLoggedIn", true)
+	return c.Next()
+}
+
+func isLoggedInErrorHandler(c *fiber.Ctx, e error) error {
+	c.Locals("isLoggedIn", false)
+	return c.Next()
 }

6762732ba2cf9c72fcf37aa542179e49a62a17ba
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2024-03-04 14:02:29 +0700

    fix: user default image

--- internal/models/user.go
+++ internal/models/user.go
@@ -9,7 +9,7 @@ type User struct {
 	Email          string         `json:"email" gorm:"not null;unique"`
 	Name           string         `json:"name" gorm:"default:''"`
 	Bio            string         `json:"bio" gorm:"default:''"`
-	Image          string         `json:"image" gorm:"default:''"`
+	Image          string         `json:"image" gorm:"default:'https://api.dicebear.com/7.x/thumbs/svg?radius=50&size=240&backgroundColor=f0f0f0&mouth=variant2&shapeColor=16a34a'"`
 	RefreshTokens  pq.StringArray `json:"-" gorm:"type:text[];default:'{}'"`
 	FollowerCount  uint           `json:"followerCount" gorm:"default:0"`
 	FollowingCount uint           `json:"followingCount" gorm:"default:0"`
